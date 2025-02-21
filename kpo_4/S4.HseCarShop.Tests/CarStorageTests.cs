e4a106fa22f1446859c3c789397265c32b90d9d3
Denis Melnik <65495593+AntiSlang@users.noreply.github.com>
2025-02-21 16:52:55 +0300

    kpo_4 tests part 1

---
+++ kpo_4/S4.HseCarShop.Tests/CarStorageTests.cs
@@ -0,0 +1,60 @@
+﻿// Мельник Денис 238
+
+using Bogus;
+using S4.HseCarShop.Models;
+using S4.HseCarShop.Models.HandCar;
+using S4.HseCarShop.Models.HybridCar;
+using S4.HseCarShop.Models.PedalCar;
+using S4.HseCarShop.Services;
+using S4.HseCarShop.Services.HandCars;
+using S4.HseCarShop.Services.HybridCars;
+using S4.HseCarShop.Services.PedalCars;
+
+namespace S4.HseCarShop.Tests;
+
+public class CarStorageTests
+{
+    private readonly Faker _faker;
+
+    public CarStorageTests()
+    {
+        _faker = new Faker();
+    }
+
+    [Fact]
+    [Trait("TestCategory", "CarStorage")]
+    public void GetCar_HaveCarsInStorage_ReturnsSuitableCar()
+    {
+        var engineType = _faker.Random.Enum<EngineType>();
+
+        var carStorage = new CarStorage();
+
+        var pedalCarFactory = new PedalCarFactory(new PedalEngineFactory());
+        var handCarFactory = new HandCarFactory(new HandEngineFactory());
+        var hybridCarFactory = new HybridCarFactory(new HybridEngineFactory());
+
+        var pedalSize = (uint)_faker.Random.Int(1, 100);
+        var pedalEngine = new PedalCarParams(pedalSize);
+
+        carStorage.AddCar(pedalCarFactory, pedalEngine);
+        carStorage.AddCar(handCarFactory, new HandCarParams());
+        carStorage.AddCar(hybridCarFactory, new HybridCarParams());
+
+        var car = carStorage.GetCar(engineType);
+
+        Assert.NotNull(car);
+        Assert.Equal(engineType, car.Engine.Type);
+    }
+
+    [Theory]
+    [InlineData(CarType.Hand)]
+    [InlineData(CarType.Pedal)]
+    [InlineData((CarType)42)]
+    [Trait("TestCategory", "CarStorage")]
+    internal void GetCar_EmptyStorage_ReturnsNull(CarType engineType)
+    {
+        var carStorage = new CarStorage();
+        var car = carStorage.GetCar((EngineType)engineType);
+        Assert.Null(car);
+    }
+}
